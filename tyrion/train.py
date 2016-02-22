9f73dfe68e83ec4855888259720ce89cd861999b
Shashank Gupta <shashank.gupta@research.iiit.ac.in>
2016-02-22 16:47:59 +0530

    minor changes to train and backprop

--- tyrion/train.py
+++ tyrion/train.py
@@ -7,7 +7,9 @@ from backprop import TrainModel
 from probarray import ProbArray
 import cPickle as pickle
 import re
+import json
 import os
+import numpy as np
 import argparse
 
 
@@ -18,9 +20,7 @@ folder = ""
 maxnum = 10000
 newdims = 100
 ntimes = 2
-lr = 0
-
-
+lr = 0.0
 def arg_parser():
     global contextSize, min_count, folder, maxnum, newdims, ntimes, lr
     parser = argparse.ArgumentParser(description='Generates word embeddings from a corpus specified by user based on paper by Lebert 2015')
@@ -31,7 +31,7 @@ def arg_parser():
     parser.add_argument('--ntimes', help='number of epochs for training', type=int)
     parser.add_argument('--learning_rate', help='learning rate for driving optimization', type=float)
     args = parser.parse_args()
-    folder = args.path
+    folder=args.path
     if args.context_size:
         contextSize = args.context_size
     else:
@@ -53,56 +53,62 @@ def arg_parser():
     else:
         lr = 0.4
 
-
 def train():
     global folder, min_count, newdims, ntimes, maxnum, lr
     lr_decay = 0.95
     arg_parser()
+    # User will enter wikipedia dump's path
+    # Parameter to remove words below this Frequenc
     pa = ProbArray()
     # Frequency to filter out low freq words
     freq = {}
-    filepaths = map(lambda x: folder + "/" + x, os.listdir(folder))
+    filepaths = map(lambda x: folder + "/" + x,os.listdir(folder))
     rgx = re.compile("([\w][\w']*\w)")
+    # Another iterator to count frequency of words
     print "Pre-processing (clearning garbage words)"
     for filepath in filepaths:
         text = open(filepath).read().lower()
         tokens = re.findall(rgx, text)
         N = len(tokens)
-        for i in xrange(0, N):
+        for i in xrange(0,N):
             if tokens[i] in freq:
                 freq[tokens[i]] += 1
             else:
                 freq[tokens[i]] = 1
 
     # Sort the frequencies storing in (freq, token) pairs and prune words with freq < min_count
-    tokenFreq = sorted(freq.items(), key=lambda x: x[1])
+    tokenFreq = sorted(freq.items(), key = lambda x: x[1])
     garbageWords = []
     for item in tokenFreq:
         if item[1] < min_count:
             garbageWords.append(item[0])
 
     print "Generating co-occurence matrix"
+    doc_text = ""
     for filepath in filepaths:
         text = open(filepath).read().lower()
         words = re.findall(rgx, text)
         N = len(words)
         temp = [' '] * (N + 2 * contextSize)
-        temp[contextSize: (contextSize + N)] = words
+        temp[contextSize : (contextSize + N)] = words
         words = temp
         for i in xrange(contextSize, (contextSize + N)):
+            # Filter out garbage words"
             if words[i] not in garbageWords:
+            # Include context size specified by user
                 for j in xrange(i-contextSize, i + contextSize+1):
-                    if i != j and words[i] != ' ' and words[j] != ' ':
+                    if i!=j and words[i] != ' ' and words[j] != ' ':
                             pa.addcontext(words[j], words[i])
                             pa.addcontext(words[i], words[j])
+
     print "Co-occurence matrix generated"
     print "Starting training"
     tm = TrainModel(maxnum, newdims)
     pa.freeze()
     for k in xrange(ntimes):
         for numwordvec in pa.getallwordvecs():
-            tm.trainonone(numwordvec[1], lr)
-        lr /= float(1+k*lr_decay)
+            tm.trainonone(numwordvec[1],lr)
+        lr /=float(1+k*lr_decay)
 
     wordembeddings = {}
     for numwordvec in pa.getallwordvecs():
@@ -116,7 +122,6 @@ def train():
     outfile = open("../embeddings.pickle", "w")
     pickle.dump(wordembeddings, outfile)
     outfile.close()
-
     print "Training completed! Embedding done."
 
 if __name__ == "__main__":
